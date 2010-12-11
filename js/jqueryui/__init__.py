be85b201dc043dcc0fa438ea64fb54f5ac09a834
Jan-Jaap Driessen <devnull@localhost>
2010-12-11 16:50:33 +0100

    Update to latest fanstatic. Update tests to not use _mode internal.

--- js/jqueryui/__init__.py
+++ js/jqueryui/__init__.py
@@ -1,35 +1,35 @@
-from fanstatic import Library, ResourceInclusion
+from fanstatic import Library, Resource
 import js.jquery
 
 library = Library('jqueryui', 'resources')
 
-jqueryui = ResourceInclusion(library, 'ui/jquery-ui.js',
+jqueryui = Resource(library, 'ui/jquery-ui.js',
     minified='ui/minified/jquery-ui.min.js',
     depends=[js.jquery.jquery])
 
-base = ResourceInclusion(library, 'themes/base/jquery-ui.css',
+base = Resource(library, 'themes/base/jquery-ui.css',
                          minified='themes/base/minified/jquery-ui.min.css')
-black_tie = ResourceInclusion(library, 'themes/black-tie/jquery-ui.css')
-blitzer = ResourceInclusion(library, 'themes/blitzer/jquery-ui.css')
-cupertino = ResourceInclusion(library, 'themes/cupertino/jquery-ui.css')
-dark_hive = ResourceInclusion(library, 'themes/dark-hive/jquery-ui.css')
-dot_luv = ResourceInclusion(library, 'themes/dot-luv/jquery-ui.css')
-eggplant = ResourceInclusion(library, 'themes/eggplant/jquery-ui.css')
-excite_bike = ResourceInclusion(library, 'themes/excite-bike/jquery-ui.css')
-flick = ResourceInclusion(library, 'themes/flick/jquery-ui.css')
-hot_sneaks = ResourceInclusion(library, 'themes/hot-sneaks/jquery-ui.css')
-humanity = ResourceInclusion(library, 'themes/humanity/jquery-ui.css')
-le_frog = ResourceInclusion(library, 'themes/le-frog/jquery-ui.css')
-mint_choc = ResourceInclusion(library, 'themes/mint-choc/jquery-ui.css')
-overcast = ResourceInclusion(library, 'themes/overcast/jquery-ui.css')
-pepper_grinder = ResourceInclusion(library, 'themes/pepper-grinder/jquery-ui.css')
-redmond = ResourceInclusion(library, 'themes/redmond/jquery-ui.css')
-smoothness = ResourceInclusion(library, 'themes/smoothness/jquery-ui.css')
-south_street = ResourceInclusion(library, 'themes/south-street/jquery-ui.css')
-start = ResourceInclusion(library, 'themes/start/jquery-ui.css')
-sunny = ResourceInclusion(library, 'themes/sunny/jquery-ui.css')
-swanky_purse = ResourceInclusion(library, 'themes/swanky-purse/jquery-ui.css')
-trontastic = ResourceInclusion(library, 'themes/trontastic/jquery-ui.css')
-ui_darkness = ResourceInclusion(library, 'themes/ui-darkness/jquery-ui.css')
-ui_lightness = ResourceInclusion(library, 'themes/ui-lightness/jquery-ui.css')
-vader = ResourceInclusion(library, 'themes/vader/jquery-ui.css')
+black_tie = Resource(library, 'themes/black-tie/jquery-ui.css')
+blitzer = Resource(library, 'themes/blitzer/jquery-ui.css')
+cupertino = Resource(library, 'themes/cupertino/jquery-ui.css')
+dark_hive = Resource(library, 'themes/dark-hive/jquery-ui.css')
+dot_luv = Resource(library, 'themes/dot-luv/jquery-ui.css')
+eggplant = Resource(library, 'themes/eggplant/jquery-ui.css')
+excite_bike = Resource(library, 'themes/excite-bike/jquery-ui.css')
+flick = Resource(library, 'themes/flick/jquery-ui.css')
+hot_sneaks = Resource(library, 'themes/hot-sneaks/jquery-ui.css')
+humanity = Resource(library, 'themes/humanity/jquery-ui.css')
+le_frog = Resource(library, 'themes/le-frog/jquery-ui.css')
+mint_choc = Resource(library, 'themes/mint-choc/jquery-ui.css')
+overcast = Resource(library, 'themes/overcast/jquery-ui.css')
+pepper_grinder = Resource(library, 'themes/pepper-grinder/jquery-ui.css')
+redmond = Resource(library, 'themes/redmond/jquery-ui.css')
+smoothness = Resource(library, 'themes/smoothness/jquery-ui.css')
+south_street = Resource(library, 'themes/south-street/jquery-ui.css')
+start = Resource(library, 'themes/start/jquery-ui.css')
+sunny = Resource(library, 'themes/sunny/jquery-ui.css')
+swanky_purse = Resource(library, 'themes/swanky-purse/jquery-ui.css')
+trontastic = Resource(library, 'themes/trontastic/jquery-ui.css')
+ui_darkness = Resource(library, 'themes/ui-darkness/jquery-ui.css')
+ui_lightness = Resource(library, 'themes/ui-lightness/jquery-ui.css')
+vader = Resource(library, 'themes/vader/jquery-ui.css')
