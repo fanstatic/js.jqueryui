6fb045faa3cfe98fbee5a35e5495dc4401a68bd4
Andreas Kaiser <disko@binary-punks.com>
2013-05-19 23:25:40 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.10.3'
+version = '1.10.4dev'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
